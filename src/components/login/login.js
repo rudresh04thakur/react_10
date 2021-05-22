0996790f770e518e20aecc2338da611d4f757c90
Rudresh L Thakur <rudresh04thakur@gmail.com>
2021-05-22 11:48:47 +0530

    pattern validation

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -4,67 +4,131 @@ import './login.scss'
 // import { bindActionCreators } from "redux";
 // import * as loginActions from "../../store/login/actions";
 export default class login extends Component {
-  
+
   constructor(props) {
-      super(props);
-      this.state = {
-        initForm:{
-          email:{value:'',isValid:true,error:''},
-          password:{value:'',isValid:true,error:''}
-        }
-      };
+    super(props);
+    this.state = {
+      initForm: {
+        email: { value: '', isValid: true, error: '' },
+        password: { value: '', isValid: true, error: '' },
+        validForm: false
+      }
+    };
   }
 
-  submitHandler = (event)=>{
+
+  submitHandler = (event) => {
     event.preventDefault();
     // for(var key in this.state.initForm){
     //   if(this.state.initForm[key]['value']==='' && this.state.initForm[key]==='object'){
     //     this.state.initForm
     //   }
-    }
-  
+  }
 
 
-  blurHandler = (event)=>{
+
+  blurHandler = (event) => {
     let key = event.target.name;
-    console.log(key)
-    if(event.target.value === ''){
-      this.setState({initForm:{
-        ...this.state.initForm,
-        [key]:{
-          isValid:false,
-          error:"This field required"
-        }
-      }})
-    }else{
-      this.setState({initForm:{
-        ...this.state.initForm,
-        [key]:{
-          isValid:true,
-          error:""
-        }
-      }})
-    }
-  }
 
-  render() {
-    return <form className="form col-md-4 col-md-offset-4">
-      <div className="input-group">
-        <label>Email</label>
-        <input name="email" onBlur={this.blurHandler.bind(this)} onChange={()=>{}} className="form-control" type="email"></input>
-        <div className="alert alert-danger" >{this.state.initForm.email.error}</div>
-      </div>
-      <div className="input-group">
-        <label>Password</label>
-        <input name="password"  onBlur={this.blurHandler.bind(this)} onChange={()=>{}} className="form-control" type="password"></input>
-        <div className="alert alert-danger" >{this.state.initForm.password.error}</div>
-      </div>
-      <div className="input-group">
-        <button className="btn btn-danger" type="reset">Reset</button>&nbsp;&nbsp;&nbsp;
-        <button className="btn btn-success" type="submit">Submit</button>
-      </div>
-    </form>
-  }
+    if (event.target.value === '') {
+      this.setState({
+        initForm: {
+          ...this.state.initForm,
+          [key]: {
+            isValid: false,
+            error: "This field required"
+          },
+          'validForm': false
+        }
+      })
+
+    } else if (event.target.type === 'email' && !(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]+$/.test(event.target.value))) {
+      this.setState({
+        initForm: {
+          ...this.state.initForm,
+          [key]: {
+            isValid: false,
+            error: "No valid email id"
+          },
+          'validForm': false
+        }
+      })
+    } else if (event.target.type === 'password' &&
+      !(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,10}$/.test(event.target.value))) {
+      this.setState({
+        initForm: {
+          ...this.state.initForm,
+          [key]: {
+            isValid: false,
+            error: "No a Valid Password. it must contain number and special char"
+          },
+          'validForm': false
+        }
+      })
+    } else {
+      this.setState({
+        initForm: {
+          ...this.state.initForm,
+          [key]: {
+            isValid: true,
+            error: ""
+          }
+        }
+      })
+    }
+    // for (key in this.state.initForm) {
+    //   if (typeof(this.state.initForm[key]) === 'object') {
+    //     if (this.state.initForm[key]['isValid'] === true) {
+    //       this.setState({
+    //         initForm: {
+    //           ...this.state.initForm,
+    //           validForm: true
+    //         }
+    //       })
+    //     } 
+    //     if(this.state.initForm[key]['isValid'] === false) {
+    //       this.setState({
+    //         initForm: {
+    //           ...this.state.initForm,
+    //           validForm: false
+    //         }
+    //       })
+    //     }
+    //   }
+
+    // }
+}
+
+render() {
+  return <form className="form col-md-4 col-md-offset-4">
+    <div className="input-group">
+      <label>Email</label>
+      <input name="email" onBlur={this.blurHandler.bind(this)} onChange={() => { }}
+        className="form-control" type="email"></input>
+      <div className="alert alert-danger" >{this.state.initForm.email.error}</div>
+    </div>
+    <div className="input-group">
+      <label>Password</label>
+      <input name="password" onBlur={this.blurHandler.bind(this)} onChange={() => { }}
+        className="form-control" type="password"></input>
+      <div className="alert alert-danger" >{this.state.initForm.password.error}</div>
+    </div>
+    <div className="input-group">
+      <button className="btn btn-danger" type="reset">Reset</button>&nbsp;&nbsp;&nbsp;
+        
+        {(() => {
+            if (this.state.initForm['validForm'] === true) {
+              return <button className="btn btn-success" type="submit" >Submit</button>
+            } else {
+              return <button className="btn btn-success" type="submit" disabled >Submit</button>
+            }
+          })()
+        }
+
+
+    </div>
+  </form>
+}
 }
 // export default connect(
 //     ({login}) => ({...login}),
